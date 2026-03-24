f8c6917c8ccd836a4a2b755f970f3cf1b048c7bc
Kurt-Kevin <kurtbadenas@gmail.com>
2026-03-25 02:51:27 +0800

    edited pythonanywhere_com_wsgi.py

--- projectsite/kurtkevin_pythonanywhere_com_wsgi.py
+++ projectsite/kurtkevin_pythonanywhere_com_wsgi.py
@@ -3,7 +3,7 @@ import sys
 
 #path where you can find the mange.py
 
-path = '/home/kurtkevin/psusenvPSUSphere/projectsite'
+path = '/home/kurtkevin/psusenv/PSUSphere/projectsite'
 if path not in sys.path:
     sys.path.insert(0, path)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projectsite.settings")
